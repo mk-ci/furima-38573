d3c75ed2b39d85137d202d5ff55e2aded67e5877
mk-ci <m7k1a2t@gmail.com>
2022-11-06 14:07:41 +0900

    モデルとテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    # Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price)
 end
---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+
+# validates :item_name
+
+# - belongs_to :user
+# - has_many :comments
+# - has_one :order
+end
