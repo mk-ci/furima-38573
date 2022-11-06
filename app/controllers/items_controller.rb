aabacfdf0ba34c422ca3666197e38df08a7772f7
mk-ci <m7k1a2t@gmail.com>
2022-11-06 23:11:51 +0900

    商品カテゴリー・状態のView作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,17 @@ class ItemsController < ApplicationController
   end
   def create
     # Item.create(item_params)
+    @aitem = Item.new(article_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,19 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
   belongs_to :user
   has_many :comments
   has_one :order
   has_one_attached :image
 
   with_options presence: true do
-    validates :item_name, :explanation, :category_id, :condition_id, :shipping_charge_id,:prefecture_id, :days_to_ship_id, :price, :user
+    validates :item_name, :explanation, :condition_id, :shipping_charge_id,:prefecture_id, :days_to_ship_id, :price, :user
+    validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   end
 
+  # def was_attached?
+  #   self.image.attached?
+  # end
 end
