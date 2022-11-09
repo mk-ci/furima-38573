16eebf870a3dc473e3459c7387dc37fab97bd217
mk-ci <m7k1a2t@gmail.com>
2022-11-09 16:54:04 +0900

    商品一覧ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-
   belongs_to :user
   # has_many :comments
-  # has_one :order
+  has_one :order
   has_one_attached :image
 
   with_options presence: true do
@@ -15,4 +14,8 @@ class Item < ApplicationRecord
               numericality: { other_than: 1, message: "can't be blank" }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :Shippingcharge
+  delegate :name, to: :shipping_charge
 end
