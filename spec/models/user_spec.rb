4241ae8b7ad09e72b29dc64cfd7dac4e7a75544c
mk-ci <m7k1a2t@gmail.com>
2022-11-17 18:47:51 +0900

    エラー文日本語表記修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -111,12 +111,12 @@ RSpec.describe User, type: :model do
       it '名字は、全角（漢字・ひらがな・カタカナ）以外では登録できない' do
         @user.last_name = 'yamada'
         @user.valid?
-        expect(@user.errors.full_messages).to include('姓（全角）名前は全角（漢字・ひらがな・カタカナ）で入力してください')
+        expect(@user.errors.full_messages).to include('姓（全角）は全角（漢字・ひらがな・カタカナ）で入力してください')
       end
       it '名前は、全角（漢字・ひらがな・カタカナ）以外では登録できない' do
         @user.first_name = 'rikutarou'
         @user.valid?
-        expect(@user.errors.full_messages).to include('名（全角）名前は全角（漢字・ひらがな・カタカナ）で入力してください')
+        expect(@user.errors.full_messages).to include('名（全角）は全角（漢字・ひらがな・カタカナ）で入力してください')
       end
       it '名前カナ(全角)名字が空では登録できない' do
         @user.last_kana = ''
@@ -131,12 +131,12 @@ RSpec.describe User, type: :model do
       it '名前カナ(全角)名字は、全角（カタカナ）以外では登録できない' do
         @user.last_kana = '山田'
         @user.valid?
-        expect(@user.errors.full_messages).to include('姓（カナ）名前は全角カタカナで入力してください')
+        expect(@user.errors.full_messages).to include('姓（カナ）は全角カタカナで入力してください')
       end
       it '名前カナ(全角)名前は、全角（カタカナ）以外では登録できない' do
         @user.first_kana = 'りくたろう'
         @user.valid?
-        expect(@user.errors.full_messages).to include("名（カナ）名前は全角カタカナで入力してください")
+        expect(@user.errors.full_messages).to include("名（カナ）は全角カタカナで入力してください")
       end
       it '生年月日が空では登録できない' do
         @user.birthday = ''
